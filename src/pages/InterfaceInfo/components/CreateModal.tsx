d5a04b86a21c67debcb267d6311fbeaf63dfbbae
WLei224 <1149630057@qq.com>
2023-07-05 00:05:59 +0800

    first commit

---
+++ src/pages/InterfaceInfo/components/CreateModal.tsx
@@ -0,0 +1,21 @@
+import type { ProColumns } from '@ant-design/pro-components';
+import { ProTable } from '@ant-design/pro-components';
+import '@umijs/max';
+import { Modal } from 'antd';
+import React from 'react';
+
+export type Props = {
+  columns: ProColumns<API.InterfaceInfo>[];
+  onCancel: () => void;
+  onSubmit: (values: API.InterfaceInfo) => Promise<void>;
+  visible: boolean;
+};
+
+const CreateModal: React.FC<Props> = (props) => {
+  const { visible, columns } = props;
+
+  return <Modal visible={visible} >
+    <ProTable type="form" columns={columns} />
+    </Modal>;
+};
+export default CreateModal;
